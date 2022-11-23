04528e04a516f04975dc705a2e1de6c2450ed010
Walter <walter.clark24@yahoo.com>
2022-11-23 13:57:57 -0500

    commit tree a0df69b3ffc04ba04a189aaa0ab8fc931e656424

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string) {
+return string.toLowerCase()
+}
+function logShout(string) {
+    console.log (string.toUpperCase())
+}
+function logWhisper(string) {
+console.log (string.toLowerCase())
+}
+function sayHiToHeadPhonedRoomate(string) {
+    if (string === string.toLowerCase())
+    return console.log ("I can't hear you!")
+}
+console.log(sayHiToHeadPhonedRoomate(string))
